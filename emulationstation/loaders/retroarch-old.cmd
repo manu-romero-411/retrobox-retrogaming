7fc4e7fef41d69660ebe3ab70e9d2a2e4aec0d36
Manuel Jesús Romero Mateos <manu4.2012@gmail.com>
2022-12-26 12:44:22 +0100

    actualizando

--- emulationstation/loaders/retroarch-old.cmd
+++ emulationstation/loaders/retroarch-old.cmd
@@ -38,23 +38,23 @@ rem echo %emu% | findstr "parallel" >NUL && (
 rem )
 
 rem ## ESTABLECER DIRECTORIOS DE GUARDADO DE PARTIDAS Y BIOS. LOS DIRECTORIOS DE PARTIDAS IRÁN SEPARADOS SEGÚN PLATAFORMA
-del %EMUDIR%\retroarch\retroarch-config.cfg
-copy %EMUDIR%\retroarch\retroarch-original.cfg %EMUDIR%\retroarch\retroarch-config.cfg
-cscript %retroboxroot%\misc\tools\replace.vbs %EMUDIR%\retroarch\retroarch-config.cfg "directorio" "%retroboxroot%\saves\%plataforma%"
-cscript %retroboxroot%\misc\tools\replace.vbs %EMUDIR%\retroarch\retroarch-config.cfg "biosdir" "%retroboxroot%\bios"
+del %EMUDIR%\retroarch-old\retroarch-config.cfg
+copy %EMUDIR%\retroarch-old\retroarch-original.cfg %EMUDIR%\retroarch-old\retroarch-config.cfg
+cscript %retroboxroot%\misc\tools\replace.vbs %EMUDIR%\retroarch-old\retroarch-config.cfg "directorio" "%retroboxroot%\saves\%plataforma%"
+cscript %retroboxroot%\misc\tools\replace.vbs %EMUDIR%\retroarch-old\retroarch-config.cfg "biosdir" "%retroboxroot%\bios"
 
 rem ## INICIAR RETROARCH
 echo %emu% | findstr "parallel" >NUL && (
-	%EMUDIR%\retroarch\retroarch-nvidia.exe -c %EMUDIR%\retroarch\retroarch-config.cfg -L %EMUDIR%\retroarch\cores\%emu%.dll %rom%
+	%EMUDIR%\retroarch-old\retroarch-nvidia.exe -c %EMUDIR%\retroarch-old\retroarch-config.cfg -L %EMUDIR%\retroarch-old\cores\%emu%.dll %rom%
 ) || (
-	%EMUDIR%\retroarch\retroarch.exe -c %EMUDIR%\retroarch\retroarch-config.cfg -L %EMUDIR%\retroarch\cores\%emu%.dll %rom%
+	%EMUDIR%\retroarch-old\retroarch.exe -c %EMUDIR%\retroarch-old\retroarch-config.cfg -L %EMUDIR%\retroarch-old\cores\%emu%.dll %rom%
 )
 
 rem ## DESHACER CONFIGURACIÓN DE DIRECTORIOS Y BIOS PARA QUE EN LA SIGUIENTE EJECUCIÓN DE RETROARCH NO HAYA CONFLICTOS
-del %EMUDIR%\retroarch\retroarch-original.cfg
-copy %EMUDIR%\retroarch\retroarch-config.cfg %EMUDIR%\retroarch\retroarch-original.cfg
-cscript %retroboxroot%\misc\tools\replace.vbs %EMUDIR%\retroarch\retroarch-original.cfg "%retroboxroot%\saves\%plataforma%" "directorio" 
-cscript %retroboxroot%\misc\tools\replace.vbs %EMUDIR%\retroarch\retroarch-original.cfg "%retroboxroot%\bios" "biosdir"
+del %EMUDIR%\retroarch-old\retroarch-original.cfg
+copy %EMUDIR%\retroarch-old\retroarch-config.cfg %EMUDIR%\retroarch-old\retroarch-original.cfg
+cscript %retroboxroot%\misc\tools\replace.vbs %EMUDIR%\retroarch-old\retroarch-original.cfg "%retroboxroot%\saves\%plataforma%" "directorio" 
+cscript %retroboxroot%\misc\tools\replace.vbs %EMUDIR%\retroarch-old\retroarch-original.cfg "%retroboxroot%\bios" "biosdir"
 
 rem ## SI SE HA NECESITADO ENCENDER, APAGAR GRÁFICA NVIDIA
 rem echo %emu% | findstr "parallel" >NUL && (
